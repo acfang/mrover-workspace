656527f5ddfb6d80eeb4eed8d4664624df928779
acfang <acfang@umich.edu>
2020-01-25 17:46:30 -0500

    test image_capture.py in full

--- onboard/kinematics/src/image_capture.py
+++ onboard/kinematics/src/image_capture.py
@@ -26,43 +26,43 @@ def main():
     # Create a Camera object
     print("beginning")
 
-    # zed = sl.Camera()
+    zed = sl.Camera()
 
-    # # Create a InitParameters object and set configuration parameters
-    # init_params = sl.InitParameters()
-    # # Use HD1080 video mode
-    # init_params.camera_resolution = sl.RESOLUTION.RESOLUTION_HD1080
-    # init_params.camera_fps = 30  # Set fps at 30
+    # Create a InitParameters object and set configuration parameters
+    init_params = sl.InitParameters()
+    # Use HD1080 video mode
+    init_params.camera_resolution = sl.RESOLUTION.RESOLUTION_HD1080
+    init_params.camera_fps = 30  # Set fps at 30
 
-    # print("camera initialized")
+    print("camera initialized")
 
-    # # Open the camera
-    # err = zed.open(init_params)
-    # if err != sl.ERROR_CODE.SUCCESS:
-    #     exit(1)
+    # Open the camera
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        exit(1)
 
-    # print("camera opened")
+    print("camera opened")
 
-    # # Capture one frame and stop
-    # image = sl.Mat()
-    # runtime_parameters = sl.RuntimeParameters()
-    # # Grab an image, a RuntimeParameters object must be given to grab()
-    # if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
-    #     # A new image is available if grab() returns SUCCESS
-    #     zed.retrieve_image(image, sl.VIEW.VIEW_LEFT)
-    #     timestamp = zed.get_timestamp(sl.TIME_REFERENCE.TIME_REFERENCE_CURRENT)
-    #     # Get the timestamp at the time the image was captured
-    #     print("Image resolution: {0} x {1} || Image timestamp: {2}\n".format(
-    #           image.get_width(), image.get_height(), timestamp))
-    #     sl.savePointCloudAs(zed, sl.POINT_CLOUD_FORMAT_PCD_ASCII,
-    #                         "surroundings.pcd", False)
-    #     subprocess.run(["scp", "surroundings.pcd",
-    #                    "mrover@10.0.0.1:base_station/kineval_stencil/dist"])
+    # Capture one frame and stop
+    image = sl.Mat()
+    runtime_parameters = sl.RuntimeParameters()
+    # Grab an image, a RuntimeParameters object must be given to grab()
+    if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+        # A new image is available if grab() returns SUCCESS
+        zed.retrieve_image(image, sl.VIEW.VIEW_LEFT)
+        timestamp = zed.get_timestamp(sl.TIME_REFERENCE.TIME_REFERENCE_CURRENT)
+        # Get the timestamp at the time the image was captured
+        print("Image resolution: {0} x {1} || Image timestamp: {2}\n".format(
+              image.get_width(), image.get_height(), timestamp))
+        sl.savePointCloudAs(zed, sl.POINT_CLOUD_FORMAT_PCD_ASCII,
+                            "surroundings.pcd", False)
+        subprocess.run(["scp", "surroundings.pcd",
+                       "mrover@10.0.0.1:base_station/kineval_stencil/dist"])
 
-    # print("ready to close")
+    print("ready to close")
 
-    # # Close the camera
-    # zed.close()
+    # Close the camera
+    zed.close()
 
 
 if __name__ == "__main__":
